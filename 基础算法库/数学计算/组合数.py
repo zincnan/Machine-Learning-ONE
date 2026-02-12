b4f9a628356d882a14f26e2aae081d3871e66533
zinc <zinc@example.com>
2026-02-12 10:55:37 +0800

    FEAT:测试组合数计算

---
+++ 基础算法库/数学计算/组合数.py
@@ -0,0 +1,7 @@
+import math
+
+p = math.perm(33,14) / (33**14)
+print(p)
+
+a=math.perm(365,60)/(365**60)
+print(a)
---
+++ 基础算法库/科学计算/sci_calculate.py
@@ -0,0 +1,8 @@
+from scipy.special import factorial
+from scipy.special import gamma
+
+a = factorial(5.5)
+
+b= gamma(6.5)
+
+print(a,b)
